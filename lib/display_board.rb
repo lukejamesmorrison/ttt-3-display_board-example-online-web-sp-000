3ea37c0549522ea4f6597370c24d276841856dc5
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-20 14:53:27 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "A Tic Tac Toe Board"
+  cell = "   "
+  horizontal_seperator = "|"
+  vertical_sperator = "-----------"
+
+  3.times  do
+    puts (cell + horizontal_seperator + cell + horizontal_seperator + cell)
+    # if(index < 2) do
+      puts vertical_seperator
+    end/
+  end
 
 end
