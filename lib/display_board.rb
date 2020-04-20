f21a0acdddcb19a62991330ccbae0e2d72e8ed1a
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-20 14:58:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,20 +11,6 @@ def display_board
     puts cell
     puts horizontal_seperator
   end
-
-  display_board()
-  # while i < 2 do
-  #
-  #   i+=1
-  # end
-
-
-
-  # 3.times do
-    # puts cell + horizontal_seperator + cell + horizontal_seperator + cell
-    # if(index < 2) do
-    # puts vertical_seperator
-    # end
-  # end
-
 end
+
+display_board
